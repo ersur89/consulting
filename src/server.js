151658cf31be684a55b492edfed0371b84d4fb80
Erick <ersur89@gmail.com>
2024-11-21 21:36:02 -0500

    consulting 0.02

---
+++ src/server.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const db = require(path.join(__dirname, 'config', 'db'));
+
+const app = express();
+const PORT = 3000;
+
+// Middleware
+app.use(express.static(path.join(__dirname, '..', 'public')));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json()); // Asegúrate de manejar JSON en el servidor
+
+// Ruta para el formulario de login
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public', 'index.html'));
+});
+
+// Ruta para el dashboard
+app.get('/dashboard', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public', 'dashboard.html'));
+});
+
+// Ruta para validar el login
+app.post('/validate-login', (req, res) => {
+    const { username, password } = req.body;
+
+    const query = 'SELECT * FROM adm_usuario WHERE usuario = ? AND clave = ?';
+    db.query(query, [username, password], (err, results) => {
+        if (err) {
+            console.error('Error al consultar la base de datos:', err);
+            return res.status(500).json({ success: false, message: 'Error interno del servidor' });
+        }
+
+        if (results.length > 0) {
+            return res.json({ success: true });
+        } else {
+            return res.json({ success: false, message: 'Credenciales incorrectas. Intenta nuevamente.' });
+        }
+    });
+});
+
+// Iniciar el servidor
+app.listen(PORT, () => {
+    console.log(`Servidor iniciado en http://localhost:${PORT}`);
+});
