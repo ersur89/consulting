e3741e6865e81909bbf57d4d0fd384b8b587045c
ersur89 <71945991+ersur89@users.noreply.github.com>
2025-02-05 13:56:18 -0500

    Update server.js

--- src/server.js
+++ src/server.js
@@ -9,6 +9,7 @@ const { createCanvas } = require('canvas');
 const PDFDocument = require('pdfkit');
 const fs = require('fs');
 const { Console } = require('console');
+const cors = require('cors'); 
 
 const db = require(path.join(__dirname, 'config', 'db'));
 
@@ -16,6 +17,7 @@ const app = express();
 const PORT = 3000;
 
 // Middleware
+app.use(cors()); // Permite solicitudes desde el frontend
 app.use(express.static(path.join(__dirname, '..', 'public')));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json()); // Asegúrate de manejar JSON en el servidor
