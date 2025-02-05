67cd5f23672f1ad30ee1c1461da2f5f4993d07fa
ersur89 <71945991+ersur89@users.noreply.github.com>
2025-02-05 12:22:15 -0500

    Conectar render con godaddy

--- src/config/db.js
+++ src/config/db.js
@@ -1,10 +1,17 @@
 const mysql = require('mysql2');
 
-const db = mysql.createConnection({
+/* const db = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: 'root',
     database: 'consulting',
+}); */
+const connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT || 3306,
 });
 
 db.connect((err) => {
